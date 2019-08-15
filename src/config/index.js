0ea1a366e326c86b3afe643228f50e21468d5bdc
okabamac <markokaba99@gmail.com>
2019-08-15 11:43:37 +0100

    ch(server):dotenv-config
    - install dotenv as dependency
    - use it in the config file for accessing env variables
    - create a .env.example file
    -  use the port key in  index.js
    [Finishes #167729778]

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,11 @@
-module.exports = {
-    secret:
-        process.env.NODE_ENV === "production" ? process.env.SECRET : "secret"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const envVariables = {
+  secret:
+        process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  port: process.env.PORT
 };
+
+export default envVariables;
