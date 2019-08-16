ac33dde35f89e43ab36ee0981b29e16a9f1cccd9
sebaztyn <sebastinechima@gmail.com>
2019-08-16 19:57:11 +0100

    chore(server): set up unit test
    install test suite for adequate testing
    
    [Starts #167729777]

--- src/index.js
+++ src/index.js
@@ -86,7 +86,15 @@ app.use((err, req, res) => {
   });
 });
 
+
+// testing route
+app.get('/', (req, res) => {
+  res.send("Welcome to Barefoot Nomad Endpoints' Page");
+});
+
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}`);
 });
+
+export default server;
