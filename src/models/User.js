f33d4f1bca763f1add5e3c28b78af287ef1eeb44
unknown <aniakuchibuike@gmail.com>
2019-08-18 19:32:17 +0100

    chores(sequelize)
    set up sequelize
    install bcrypt and sequelize cli
    [finish #167729772]

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,12 @@
-
-export default (sequelize, DataTypes) => {
+'use strict';
+module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {});
-  User.associate = function (models) {
+  User.associate = function(models) {
     // associations can be defined here
   };
   return User;
