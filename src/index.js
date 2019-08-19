3afca1c354042cbec35bc76ec138104fa21ac8fc
okabamac <markokaba99@gmail.com>
2019-08-19 12:39:33 +0100

    ch(docker): docker-configuration - Add dockerfile script for multistage build - Add docker-compose script to ensure easy build and service running - Delete .env.sample file - Add .travis.yml file to run docker in travis - Rewrite test/dev commands in package.json [Finishes #167729782]

--- src/index.js
+++ src/index.js
@@ -45,12 +45,12 @@ if (!isProduction) {
   app.use(errorhandler());
 }
 
-if (isProduction) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+// if (isProduction) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect('mongodb://localhost/conduit');
+//   mongoose.set('debug', true);
+// }
 
 app.use(apiRoutes);
 
