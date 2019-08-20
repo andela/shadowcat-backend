3f52fda99e6bf2fb948ec134d274d93eb44ff855
unknown <aniakuchibuike@gmail.com>
2019-08-20 11:31:24 +0100

    chores(sequelize) setup sequelize install sequelize and bcrypt
    [finish #167729772]

--- src/database/migrations/20190818182539-create-user.js
+++ src/database/migrations/20190818182539-create-user.js
@@ -6,31 +6,31 @@ export default {
       autoIncrement: true,
       primaryKey: true,
       type: Sequelize.INTEGER
-      },
+    },
     firstname: {
       type: Sequelize.STRING,
       allowNull: false,
-      },
+    },
     lastname: {
       type: Sequelize.STRING,
       allowNull: false,
-      },
+    },
     email: {
       type: Sequelize.STRING,
       allowNull: false,
-      },
+    },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
-      },
+    },
     createdAt: {
       allowNull: false,
       type: Sequelize.DATE
-   },
+    },
     updatedAt: {
       allowNull: false,
       type: Sequelize.DATE
-   }
+    }
     }),
   down: queryInterface => queryInterface.dropTable('Users')
 };
