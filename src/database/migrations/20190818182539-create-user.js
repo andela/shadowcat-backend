1c4f5860b61580e4e4a950d8da7803b272e668bd
unknown <aniakuchibuike@gmail.com>
2019-08-20 11:20:57 +0100

    chores(sequelize) setup sequelize install sequelize and bcrypt
    [finish #167729772]

--- src/database/migrations/20190818182539-create-user.js
+++ src/database/migrations/20190818182539-create-user.js
@@ -1,35 +1,35 @@
 
 export default {
   up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
+   id: {
+     allowNull: false,
+     autoIncrement: true,
+     primaryKey: true,
+     type: Sequelize.INTEGER
       },
-    firstname: {
-      type: Sequelize.STRING,
-      allowNull: false,
+   firstname: {
+     type: Sequelize.STRING,
+     allowNull: false,
       },
-    lastname: {
-      type: Sequelize.STRING,
-      allowNull: false,
+   lastname: {
+     type: Sequelize.STRING,
+     allowNull: false,
       },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
+   email: {
+     type: Sequelize.STRING,
+     allowNull: false,
       },
-    password: {
-      type: Sequelize.STRING,
-    allowNull: false,
+   password: {
+     type: Sequelize.STRING,
+     allowNull: false,
       },
-      createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
+   createdAt: {
+     allowNull: false,
+     type: Sequelize.DATE
    },
-     updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
+   updatedAt: {
+     allowNull: false,
+     type: Sequelize.DATE
    }
     }),
   down: queryInterface => queryInterface.dropTable('Users')
