62789e71df3e14c82e05378d0e7b0698ad8e6ae2
unknown <aniakuchibuike@gmail.com>
2019-08-23 00:35:40 +0100

    feature (login) user login endpoint [finish #167729787]

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -6,35 +6,34 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('User login', () => {
-  
   it('should login a user account on /login POST ', (done) => {
     chai.request(server)
-    .post('/api/auth/login')
-    .send({
-      email: 'chimamark@example.com',
-      password: 'Chibyke8%'
-    })
-    .end((err, res) => {
-      expect(res.status).to.equal(201);
-      expect(res.body.payload).to.be.an('object');
-      expect(res.body).to.have.property('status');
-      expect(res.body.message).to.equal('User successfully logged in');
-      done();
-    });
-});
+      .post('/api/auth/login')
+      .send({
+        email: 'chimamark@example.com',
+        password: 'Chibyke8%'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.payload).to.be.an('object');
+        expect(res.body).to.have.property('status');
+        expect(res.body.message).to.equal('User successfully logged in');
+        done();
+      });
+  });
 
-it('should return an invalid login', (done) => {
-  chai.request(server)
+  it('should return an invalid login', (done) => {
+    chai.request(server)
       .post('/api/auth/login')
       .send({
         email: 'chimamark@example.com',
         password: '1234d567'
       })
-  .end((err, res) => {
-    expect(res.status).to.equal(400);
-    expect(res.body).to.be.an('object');
-    expect(res.body.message).to.equal('Invalid email or password');
-    done();
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body.message).to.equal('Invalid email or password');
+        done();
+      });
   });
-  })
 });
