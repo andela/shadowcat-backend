79259919ae625acbe9248a463e8f058076d20a9f
unknown <aniakuchibuike@gmail.com>
2019-08-23 11:09:51 +0100

    features (login) user login endpoint
    [finish #167729787]

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,11 +42,9 @@ class UserController {
         id: user.id,
         email: user.email
       };
-      jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, (err, token) => {
-        return res.status(201).json({
+      jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, (err, token) =>  res.status(201).json({
           status: 'success', message: 'User successfully logged in', payload, token
-        });
-      });
+        }));
     } catch (error) {
       next(error);
     }
