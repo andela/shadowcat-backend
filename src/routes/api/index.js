4d83e1625ecb6452b7834bfca44a2b4e8968d6ad
unknown <aniakuchibuike@gmail.com>
2019-08-26 12:30:43 +0100

    feature(login) setup login endpoint [finish #167729787]

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -7,7 +7,7 @@ const usersRouter = express.Router();
 
 usersRouter.get('/', (request, response) => response.status(200).send('Welcome to  Shadowcat API'));
 
-usersRouter.use('/auth', authRouter);
+usersRouter.use('/v1/auth', authRouter);
 
 usersRouter.use((req, res) => {
   res.status(404).json({
--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -2,18 +2,11 @@ import { check } from 'express-validator';
 import Errors from './errorMessage';
 
 const loginValidation = [
-  check('email')
-    .exists({ checkFalsy: true })
-    .withMessage('Input a valid email address')
-    .isEmail()
-    .withMessage('Input a valid email address')
-    .matches(/@andela.com$/)
-    .withMessage('nonandela email'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Input password')
-    .isLength({ min: 8 })
-    .withMessage('Input longer password'),
+  check('email').isEmail().trim().withMessage('Input a valid email address'),
+  check('email').not().isEmpty().withMessage('Input email address'),
+  check('email').matches(/@andela.com$/).withMessage('Input an Andela email'),
+  check('password').not().isEmpty().withMessage('Input password'),
+  check('password').isLength({ min: 8 }).withMessage('Input longer password'),
   Errors.displayErrs,
 ];
 
