2d20456340866386879554a6915e7e8b42f0cc3b
unknown <aniakuchibuike@gmail.com>
2019-08-27 12:48:04 +0100

    feature(login) setup login endpoint [finish #167729787]

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,21 +1,29 @@
-import { check } from 'express-validator';
+import { check, validationResult } from 'express-validator';
+import pullErrors from '../utils/helper/pulledError';
 import { loginErrors } from '../utils/errorMessage';
-import Errors from './errorMessage';
 
 const loginValidation = [
   check('email')
     .exists({ checkFalsy: true })
-    .withMessage(`email: ${loginErrors.undefinedEmail}`)
+    .withMessage(`email ${loginErrors.undefinedEmail}`)
     .isEmail()
-    .withMessage(`email: ${loginErrors.invalidEmail}`)
+    .withMessage(`email ${loginErrors.invalidEmail}`)
     .matches(/@andela.com$/)
-    .withMessage(`email: ${loginErrors.nonAndelanEmail}`),
+    .withMessage(`email ${loginErrors.nonAndelanEmail}`),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage(`password: ${loginErrors.undefinedPassword}`)
-    .isLength({ min: 8 })
-    .withMessage(`password: ${loginErrors.invalidPassword}`),
-  Errors.displayErrs,
+    .withMessage(`password ${loginErrors.undefinedPassword}`), 
+  async (req, res, next) => {
+    const { errors } = validationResult(req);
+    if (errors.length) {
+      const pulledErrors = pullErrors(errors);
+      return res.status(400).json({
+        status: 400,
+        error: pulledErrors
+      });
+    }
+    return next();
+  }
 ];
 
 export default loginValidation;
