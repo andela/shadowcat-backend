6a6af150de1b260cfcfaddbaed7199f52136d39d
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-08-28 23:30:31 +0100

    feature(email-verify):format route index

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,28 +1,8 @@
 import express from 'express';
-import passport from 'passport';
 import authRouter from './auth';
 
 const index = express.Router();
 
 index.use('/v1/auth', authRouter);
-index.post('/users/login', (req, res, next) => {
-  if (!req.body.user.email) {
-    return res.status(422).json({ errors: { email: "can't be blank" } });
-  }
-
-  if (!req.body.user.password) {
-    return res.status(422).json({ errors: { password: "can't be blank" } });
-  }
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-
-    if (user) {
-      return res.json({ user: user.toAuthJSON() });
-    }
-    return res.status(422).json(info);
-  })(req, res, next);
-});
 
 export default index;
