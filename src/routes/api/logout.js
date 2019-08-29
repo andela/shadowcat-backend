cd99800b10859e99096533c597bf281a12bf0eed
David Frank <davidfrank96.df@gmail.com>
2019-08-28 21:00:12 -0800

    feat(feedback) - changing the GET request to POST request [Finishes #167729790]

--- src/routes/api/logout.js
+++ src/routes/api/logout.js
@@ -5,6 +5,6 @@ import Logout from '../../controllers/logout';
 const router = Router();
 const { logout } = Logout;
 
-router.get('/v1/auth/logout', logout);
+router.post('/v1/auth/logout', logout);
 
 export default router;
