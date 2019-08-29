d0f399dcf66887d703a89b61cba92ba3015499c0
David Frank <davidfrank96.df@gmail.com>
2019-08-28 22:46:03 -0800

    feat(spaces) -removing sapces [Finishes#167729790]

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 import socialMediaRoutes from './socialmedia';
 import authRouter from './auth';
 import logoutRoute from './logout';
@@ -7,7 +6,6 @@ import passwordRouter from './passwordResetRoute';
 
 const usersRouter = express.Router();
 usersRouter.use('/', socialMediaRoutes);
-
 
 usersRouter.use('/', logoutRoute);
 usersRouter.get('/', (request, response) => response.status(200).send('Welcome to  Shadowcat API'));
