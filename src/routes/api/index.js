528b0c6cafb21af93745cce39e3b17b3e58ea7ef
unknown <aniakuchibuike@gmail.com>
2019-09-05 19:08:33 +0100

    features(profilePageSetting) Set up user's profile page setting [finish #167729791]

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -6,16 +6,8 @@ import passwordRouter from './passwordResetRoute';
 const usersRouter = express.Router();
 
 usersRouter.use('/v1/auth', authRouter);
-<<<<<<< HEAD
 usersRouter.use('/v1/users/profile', profileRoute);
 usersRouter.use('/v1/users', passwordRouter);
-=======
-usersRouter.use('/', profileRoute);
-usersRouter.use('/v1/users', passwordRouter);
-
-usersRouter.get('/', (req, res) => res.status(200).send('Welcome to  Shadowcat API'));
-
->>>>>>> staging
 
 usersRouter.use((req, res) => {
   res.status(404).json({
