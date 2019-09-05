1a5b40478436e4f7607820851e466d8ce3635a3a
unknown <aniakuchibuike@gmail.com>
2019-09-05 10:33:55 +0100

    features(profilePageSetting) set up user profile setting [finish #167729791]

--- src/validation/profilevalidation.js
+++ src/validation/profilevalidation.js
@@ -38,7 +38,7 @@ const profileValidation = [
     .exists({ checkFalsy: true })
     .withMessage(`birthday ${userProfileErrors.undefinedBirthday}`)
     // https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy
-    .matches(/^((((0[13578])|([13578])|(1[02]))[\/](([1-9])|([0-2][0-9])|(3[01])))|(((0[469])|([469])|(11))[\/](([1-9])|([0-2][0-9])|(30)))|((2|02)[\/](([1-9])|([0-2][0-9]))))[\/]\d{4}$|^\d{4}$/)
+    .matches(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/)
     .withMessage(`birthday ${userProfileErrors.invalidBirthday}`),
   check('preferredlanguage')
     .optional()
