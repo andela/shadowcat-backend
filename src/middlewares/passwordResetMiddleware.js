eb86c0393ec84240c49b6a5dd9ab6b7d602880e0
Okafor Chidimma <chidimma.okafor.c@gmail.com>
2019-09-09 13:09:24 +0100

    feature(Notification):Implement Notification feature
    [Finishes 167729796]

--- src/middlewares/passwordResetMiddleware.js
+++ src/middlewares/passwordResetMiddleware.js
@@ -73,26 +73,6 @@ class authenticator {
  * @returns {Method} Calls next method on success
  * @memberof authenticator
  */
-  static async isLoggedIn(req, res, next) {
-    let msg;
-    try {
-      const requestToken = req.headers['x-auth-token'];
-      if (!requestToken) {
-        msg = 'Access denied.Unauthorized request. Please Log In';
-        return res.status(401).json(errorResponse(msg));
-      }
-      const verifiedToken = await verifyToken(requestToken);
-      if (!verifiedToken) {
-        msg = 'Access denied.Unauthorized request. Please Log In';
-        return res.status(401).json(errorResponse(msg));
-      }
-      req.body.token = verifiedToken;
-      return next();
-    } catch (error) {
-      msg = 'Access denied.Unauthorized request. Please Log In';
-      return res.status(401).json(errorResponse(msg));
-    }
-  }
 }
 
 export default authenticator;
