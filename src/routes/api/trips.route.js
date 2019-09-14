6f29edb27276b77e1007779283b93416d83c8775
David Frank <davidfrank96.df@gmail.com>
2019-09-14 09:40:39 -0800

    feat(returnTrip) -implemented validation for User input -wrote test for returnTrip - added a new seed to the database for test [Finishes#167729793]

--- src/routes/api/trips.route.js
+++ src/routes/api/trips.route.js
@@ -2,11 +2,8 @@ import express from 'express';
 import { Trips } from '../../controllers';
 import { Authentication } from '../../middlewares';
 import {
-  onewayCheck,
-  onewayValidateInput,
-  multicityCheck,
-  multicityValidateInput,
-  userRequestHistoryValidator
+  // eslint-disable-next-line max-len
+  onewayCheck, onewayValidateInput, returnTripCheck, returnTripValidateInput, multicityCheck, multicityValidateInput, userRequestHistoryValidator
 } from '../../validation';
 import { validate, validateRequestType } from '../../utils/helper/tripTypeChecker';
 
@@ -17,7 +14,7 @@ const router = express.Router();
 
 router
   .post('/request', authenticate, validateRequestType, validate(onewayCheck(), 'one-way'),
-    onewayValidateInput, validate(multicityCheck(), 'Multi-city'), multicityValidateInput, tripRequest)
+    onewayValidateInput, validate(returnTripCheck(), 'Return-Trip'), returnTripValidateInput, validate(multicityCheck(), 'Multi-city'), multicityValidateInput, tripRequest)
 
   .get('/get_trips/:id', getManagerTrips)
 
