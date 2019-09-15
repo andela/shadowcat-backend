5d46ca8860eca723a1b14dfb643ffd1160eb2cb8
David Frank <davidfrank96.df@gmail.com>
2019-09-14 17:14:36 -0800

    (Feedbacks) - implemented feedbacks [Finishes#167729793]

--- src/routes/api/trips.route.js
+++ src/routes/api/trips.route.js
@@ -2,8 +2,10 @@ import express from 'express';
 import { Trips } from '../../controllers';
 import { Authentication } from '../../middlewares';
 import {
-  // eslint-disable-next-line max-len
-  onewayCheck, onewayValidateInput, returnTripCheck, returnTripValidateInput, multicityCheck, multicityValidateInput, userRequestHistoryValidator
+  onewayCheck, onewayValidateInput,
+  returnTripCheck, returnTripValidateInput,
+  multicityCheck, multicityValidateInput,
+  userRequestHistoryValidator
 } from '../../validation';
 import { validate, validateRequestType } from '../../utils/helper/tripTypeChecker';
 
@@ -14,7 +16,9 @@ const router = express.Router();
 
 router
   .post('/request', authenticate, validateRequestType, validate(onewayCheck(), 'one-way'),
-    onewayValidateInput, validate(returnTripCheck(), 'Return-Trip'), returnTripValidateInput, validate(multicityCheck(), 'Multi-city'), multicityValidateInput, tripRequest)
+    onewayValidateInput,
+    validate(returnTripCheck(), 'Return-Trip'), returnTripValidateInput,
+    validate(multicityCheck(), 'Multi-city'), multicityValidateInput, tripRequest)
 
   .get('/get_trips/:id', getManagerTrips)
 
